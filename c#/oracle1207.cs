cc2afb76fcc7ff71c71bdd9cd5b24d568481b2e0
Justin_Kim <65004984+justin-kim3000@users.noreply.github.com>
2022-12-07 16:55:19 +0900

    create drop update table

--- c#/oracle1207.cs
+++ c#/oracle1207.cs
@@ -5,18 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace oracle1206
+namespace oracle1207
 {
     internal class Program
     {
-
         static string ORA_ADDR =
-           "Data Source=(DESCRIPTION=(ADDRESS_LIST=" +
-           "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)" +
-           "(PORT=1521)))" +
-           "(CONNECT_DATA=(SERVER=DEDICATED)" +
-           "(SERVICE_NAME=XE)));" +
-           "User Id=test;Password=1234;";
+                   "Data Source=(DESCRIPTION=(ADDRESS_LIST=" +
+                   "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)" +
+                   "(PORT=1521)))" +
+                   "(CONNECT_DATA=(SERVER=DEDICATED)" +
+                   "(SERVICE_NAME=XE)));" +
+                   "User Id=test;Password=1234;";
         // 오라클 접속, sql명령어 객체 생성
         static OracleConnection conn = new OracleConnection(ORA_ADDR);
         static OracleCommand cmd = new OracleCommand();
@@ -24,9 +23,6 @@ namespace oracle1206
 
         static void Main(string[] args)
         {
-            // 1. 이름 입력받아 delete
-            // 2. 이름,
-
             // employee, department, salgrade 테이블 생성
             // 메뉴 생성
 
@@ -51,22 +47,23 @@ namespace oracle1206
                 Console.WriteLine("-----------------------------------------------------------");
                 Console.WriteLine("DBMS 관리 APP v1.0");
                 Console.WriteLine("-----------------------------------------------------------");
+
                 Console.WriteLine("1. 테이블 생성");
                 Console.WriteLine("2. 테이블 삭제");
                 Console.WriteLine("3. 랜덤 데이터 삽입");
                 Console.WriteLine("4. 테이블 입력");
                 Console.WriteLine("5. 테이블 보기");
                 Console.WriteLine("6. 이름 검색 후 삭제");
-                Console.WriteLine("7. 이름 검색 후 정보 수정");
 
-
-                Console.WriteLine("8. 종료");
+                Console.WriteLine("7. 종료");
 
                 string a = Console.ReadLine();
                 int menu = Convert.ToInt32(a);
                 if (menu == 1)
                 {
-                    createTable();
+                    Console.Write("생성할 테이블 이름: ");
+                    string name = Console.ReadLine();
+                    createTable(name);
                 }
                 else if (menu == 2)
                 {
@@ -96,23 +93,11 @@ namespace oracle1206
                     string name = Console.ReadLine();
                     delNameData(name);
                 }
-                else if (menu == 7)
-                {
-                    Console.Write("찾을 이름 입력: ");
-                    string find_name = Console.ReadLine();
-                    Console.Write("변경할 이름 입력: ");
-                    string chan_name = Console.ReadLine();
-                    Console.Write("변경할 나이 입력: ");
-                    int chan_age = int.Parse(Console.ReadLine());
-                    Console.Write("변경할 주소 입력: ");
-                    string chan_adrr = Console.ReadLine();
-                    chanNameData(find_name, chan_name, chan_age,chan_adrr);
-                }
-                else if (menu == 8) Environment.Exit(0);
+                else if (menu == 7) Environment.Exit(0);
             }
         }
 
-        static void connectDB() 
+        static void connectDB()
         {
             // try ~ catch : 각체지향 프로그래밍에서 예외처리
             try
@@ -122,12 +107,12 @@ namespace oracle1206
             }
             catch (OracleException e)
             {
-                Console.WriteLine("접속에러: "+e.Message);
-                
+                Console.WriteLine("접속에러: " + e.Message);
+
             }
 
         }
-        static void disconnectDB() 
+        static void disconnectDB()
         {
             try
             {
@@ -143,11 +128,17 @@ namespace oracle1206
             }
         }
 
-        static void createTable() 
+        static void createTable(string name)
         {
             try
             {
-                string sql = "create table stud_t(\r\n    id number not null,\r\n    name varchar2(20) not null,\r\n    age number not null,\r\n    addr varchar2(50) not null,\r\n    CONSTRAINT pk_stud_t_id primary key(id))";
+                string sql = "create table " +
+                    name +
+                    "(id number not null,name varchar2(20) not null,age number not null," +
+                    "addr varchar2(50) not null,CONSTRAINT " +
+                    "pk_" +
+                    name +
+                    " primary key(id))";
                 cmd.Transaction = conn.BeginTransaction();
                 cmd.Connection = conn;
                 cmd.CommandText = sql;
@@ -156,12 +147,12 @@ namespace oracle1206
             }
             catch (OracleException e)
             {
-                Console.WriteLine("테이블 삭제: " + e.Message);
+                Console.WriteLine("테이블 생성 오류: " + e.Message);
                 cmd.Transaction.Rollback();
 
             }
         }
-        static void deleteTable() 
+        static void deleteTable()
         {
             try
             {
@@ -184,7 +175,7 @@ namespace oracle1206
         {
             Random rand = new Random();
 
-            string[] first_name = { "김", "나","박", "이","최","문","권" };
+            string[] first_name = { "김", "나", "박", "이", "최", "문", "권" };
             string[] sec_name = { "성", "기", "준", "구", "슬", "나", "샛" };
             string[] end_name = { "별", "용", "수", "완", "혁", "리", "람" };
 
@@ -193,9 +184,9 @@ namespace oracle1206
 
 
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                int pick = rand.Next(1000);
+                int pick = rand.Next(10000);
                 int pick_f = rand.Next(first_name.Length);
                 int pick_s = rand.Next(sec_name.Length);
                 int pick_e = rand.Next(end_name.Length);
@@ -266,7 +257,7 @@ namespace oracle1206
         {
             try
             {
-                string sql = "INSERT INTO stud_t VALUES (" + id + ",'"+ name+"',"+ age+",'"+ adrr+"')";
+                string sql = "INSERT INTO stud_t VALUES (" + id + ",'" + name + "'," + age + ",'" + adrr + "')";
                 cmd.Transaction = conn.BeginTransaction();
                 cmd.Connection = conn;
                 cmd.CommandText = sql;
@@ -279,14 +270,14 @@ namespace oracle1206
                 cmd.Transaction.Rollback();
 
             }
-         
+
         }
 
         static void delNameData(string name)
         {
             try
             {
-                string sql = "DELETE FROM stud_t WHERE name = '" + name +"'";
+                string sql = "DELETE FROM stud_t WHERE name = '" + name + "'";
                 cmd.Transaction = conn.BeginTransaction();
                 cmd.Connection = conn;
                 cmd.CommandText = sql;
@@ -301,32 +292,7 @@ namespace oracle1206
             }
 
         }
-        static void chanNameData(string find_name, string chan_name, int chan_age, string chan_adrr)
-        {
-            try
-            {
-                string sql = "update stud_t set name = '" +
-                    chan_name +
-                    "', age = " +
-                    chan_age +
-                    ", addr = '" +
-                    chan_adrr +
-                    "' where name = '" +
-                    find_name +
-                    "'";
-                cmd.Transaction = conn.BeginTransaction();
-                cmd.Connection = conn;
-                cmd.CommandText = sql;
-                cmd.ExecuteNonQuery();
-                cmd.Transaction.Commit();
-            }
-            catch (OracleException e)
-            {
-                Console.WriteLine("테이블 입력: " + e.Message);
-                cmd.Transaction.Rollback();
 
-            }
-
-        }
     }
 }
+
